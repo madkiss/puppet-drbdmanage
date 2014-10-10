18c1e0c992db29232e4e9dcd59571fa23810ed67
Martin Gerhard Loschwitz <m.loschwitz@syseleven.de>
2014-10-10 14:47:36 +0200

    Initial check_in for the node_join fact

---
+++ lib/facter/node_join.rb
@@ -0,0 +1,22 @@
+﻿# node_join.rb
+nodes = []
+output = Puppet::Util::Execution.execute("drbdmanage nodes -m")
+
+String(output).each_line do |s|
+  line = s.to_s
+  line.split(/\s*,\s*/)
+  nodes.push $1
+end
+
+nodes.each do |node|
+  
+  output = Puppet::Util::Execution.execute("drbdmanage howto-join #{node}")
+  fact_name = "#{node}_join"
+  
+  Facter.add(:fact_name) do
+    setcode do
+      output
+    end
+  end
+
+end
