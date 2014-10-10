ffad1d0a7b0c185d8d5ad0dd66ad29290b58c8e4
Martin Gerhard Loschwitz <m.loschwitz@syseleven.de>
2014-10-10 10:41:37 +0200

    Initial checkin of the module for the master node

---
+++ manifests/master.pp
@@ -0,0 +1,58 @@
+#   Copyright 2014 SysEleven GmbH
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   Author: Martin Loschwitz <m.loschwitz@syseleven.de>
+
+# === Parameters:
+#
+# [*physical_volume*]
+#   (required) The physical volume that drbdmanage is supposed to use
+#
+# [*node_ip*]
+#   (required) The IP this node will use when talking to other nodes
+#
+# [*poolname*]
+#   (optional) The name of the PV to be used by drbdmanage
+#   Default: drbdpool
+#
+# [*cluster_nodes*]
+#   (required) An array of hosts to be part of this drbdmanage cluster
+
+class drbdmanage::master(
+  $physical_volume = $drbdmanage::params::physical_volume,
+  $master_ip = $drbdmanage::params::node_ip,
+  $vg_name = $drbdmanage::params::poolname,
+  $cluster_nodes = $drbdmanage::params::cluster_nodes,
+) inherits drbdmanage::params {
+
+  include lvm
+
+  physical_volume { $physical_volume:
+    ensure => present,
+  }
+
+  volume_group { $poolname:
+    ensure           => present,
+    physical_volumes => $physical_volume,
+  }
+
+## Create the drbdmanaged master instance
+
+  exec { "drbd-init":
+    path    => "/sbin:/usr/bin:/usr/sbin:/bin",
+    command => "drbdmanage init --quiet $master_ip",
+    unless  => "`vgdisplay -s $vg_name`",
+  }
+
+}
