6f86b5c91c3b26aa54e037ac82a601e93528eb6c
Martin Gerhard Loschwitz <m.loschwitz@syseleven.de>
2014-10-10 16:23:53 +0200

    Second version of this custom fact, now behaving properly

--- lib/facter/node_join.rb
+++ lib/facter/node_join.rb
@@ -1,21 +1,39 @@
+#   Copyright 2014 SysEleven GmbH
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#   implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   Author: Martin Loschwitz <m.loschwitz@syseleven.de>
+
 # node_join.rb
+
+require 'puppet'
+
 nodes = []
 output = Puppet::Util::Execution.execute("drbdmanage nodes -m")
 
 String(output).each_line do |s|
-  line = s.to_s
-  line.split(/\s*,\s*/)
-  nodes.push $1
+    nodes << s[/(.*?),/,1]
 end
 
 nodes.each do |node|
-  
-  output = Puppet::Util::Execution.execute("drbdmanage howto-join #{node}")
+
+  output = Puppet::Util::Execution.execute("drbdmanage howto-join #{node} 2>/dev/null")
   fact_name = "#{node}_join"
-  
-  Facter.add(:fact_name) do
+
+  Facter.add("#{fact_name}") do
     setcode do
-      output
+      output.chop
     end
   end
 
